fc43831fe2e6e827d4810ac8a876c48907f2ccb0
metalentco <metalentco777@gmail.com>
2020-06-02 17:29:53 +0100

    changed to new heroku database

--- heroku_db.js
+++ heroku_db.js
@@ -1,11 +1,11 @@
 const Pool = require("pg").Pool;
 
 const pool = new Pool({
-  user: "hotutuuzhahype",
-  host: "ec2-52-70-15-120.compute-1.amazonaws.com",
+  user: "nlindowrieebgu",
+  host: "ec2-52-7-39-178.compute-1.amazonaws.com",
   password: process.env.SECRET,
   port: 5432,
-  database: "d8k6m96qtem9pq",
+  database: "dfkojka2mlteeu",
 });
 
 module.exports = pool;
